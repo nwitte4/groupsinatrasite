9b485df3537bebcdf574c0ec075d05f63c1fe22d
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-06 14:46:21 -0500

    add SendGrid mailer

--- Gemfile
+++ Gemfile
@@ -4,9 +4,12 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'pry', group: :development
+
 # gem "rails"
 gem 'sinatra'
 gem 'sinatra-reloader'
 gem 'rspec'
 gem 'rack'
 gem 'rack-test'
+gem 'sendgrid-ruby'
--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sendgrid-ruby'
+require 'pry'
+
+include SendGrid
 
 get '/' do
   erb :index
@@ -11,4 +15,18 @@ end
 
 get '/menu' do
   erb :menu
+end
+
+post '/' do
+  binding.pry
+  from = Email.new(email: params['email'])
+  to = Email.new(email: 'alessandro.braidotti@gmail.com')
+  subject = 'Sending with SendGrid is Fun'
+  content = Content.new(type: 'text/plain', value: params['body'])
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  erb :index
 end
