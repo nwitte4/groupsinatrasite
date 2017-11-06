10033fa604b48b5aae17062e71161c103487919e
Nicole W <nicolemairead4@gmail.com>
2017-11-06 11:58:50 -0500

    add drink function

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,17 @@
-console.log('Hello world');
+var form = document.getElementById('drink-form');
+var drinkList = document.getElementById('drinkBox');
 
+form.addEventListener('submit', function(event){
+  event.preventDefault();
 
+  // get value from input
+  let drink = event.target.drink;
+  let description = event.target.description;
+  // build li tag with value
+  let newDrink = '<pre class="language-javascript"><code class="language-javascript"><div class="drink-title"><strong>' + drink.value + "</strong></div><div class='drink-desc'>" + description.value + "</div></code></pre>";
+  // add li to ul
+  drinkList.innerHTML += newDrink;
 
-
-
-
-// contact page js below
+  drink.value = '';
+  description.value = '';
+});
